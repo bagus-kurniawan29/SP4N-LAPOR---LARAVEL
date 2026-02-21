9088f5438a35c707200029c448342705f972326d
bagus-kurniawan29 <contdbk29@gmail.com>
2026-02-21 22:02:42 +0800

    try chat realtime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+public function boot(): void
+{
+   
+    \Illuminate\Support\Facades\Broadcast::routes();
+
+    require base_path('routes/channels.php');
+}
 }
--- routes/channels.php
+++ routes/channels.php
@@ -6,5 +6,5 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 Broadcast::channel('laporan.{id}', function ($user, $id) {
-    return auth()->check(); 
+    return auth()->check();
 });
